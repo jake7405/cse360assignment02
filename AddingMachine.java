47f52bf99b089d54d84b57554cda867352ef69ad
Jake Blackstone <jakeblackstone@ThiccMacc-3.local>
2020-10-03 20:59:09 -0700

    Added javadoc comments

---
+++ AddingMachine.java
@@ -0,0 +1,59 @@
+package cse360assignment02; // assignment pkg
+
+/**
+ * @author Jacob Blackstone
+ * @version 1.0
+ */
+
+ /** Class that contains various arithmetic operations */
+public class AddingMachine {
+  private int total;
+  
+  /** Constructor, sets total to 0
+   *  @return AddingMachine object
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+   * @return int value of total
+   */
+  public int getTotal () {
+  }
+  
+  /** 
+   * @return void
+   * @param value integer to be added to total
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * @return void
+   * @param value integer to be subtracted from total
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * @return String of operations performed
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * @return void
+   * Clears "memory" (String of operations)
+   */
+  public void clear() {
+  }
+
+  /** Main method */
+  public static void main(String[] args){
+   
+    
+  }
+}
+
