ebe0baeb09e793b237c9b8e8e4efdb50db959ab5
Jake Blackstone <jakeblackstone@ThiccMacc-3.local>
2020-10-04 16:31:09 -0700

    Implemented required functions and variables

--- AddingMachine.java
+++ AddingMachine.java
@@ -8,18 +8,22 @@ package cse360assignment02; // assignment pkg
  /** Class that contains various arithmetic operations */
 public class AddingMachine {
   private int total;
+  private String opHistory;
   
-  /** Constructor, sets total to 0
+  /** 
+   * Constructor, sets total to 0
    *  @return AddingMachine object
    */
   public AddingMachine () {
-    total = 0;  // not needed - included for clarity
+    total = 0;
+    opHistory = "0"; 
   }
   
   /**
    * @return int value of total
    */
-  public int getTotal () {
+  public int getTotal() {
+    return total;
   }
   
   /** 
@@ -27,6 +31,8 @@ public class AddingMachine {
    * @param value integer to be added to total
    */
   public void add (int value) {
+    total += value;
+    opHistory += " + " + value;
   }
 
   /**
@@ -34,13 +40,15 @@ public class AddingMachine {
    * @param value integer to be subtracted from total
    */
   public void subtract (int value) {
+    total -= value;
+    opHistory += " - " + value;
   }
 
   /**
    * @return String of operations performed
    */
   public String toString () {
-    return "";
+    return opHistory;
   }
 
   /**
@@ -48,12 +56,36 @@ public class AddingMachine {
    * Clears "memory" (String of operations)
    */
   public void clear() {
+    System.out.println("MEMORY CLEARED");
+    opHistory = "0";
   }
 
   /** Main method */
   public static void main(String[] args){
-   
-    
+   AddingMachine am = new AddingMachine();
+
+    /** Test code */
+   int i = 1;
+   while(i <= 5){
+    am.add(i);
+    i++;
+   }
+
+   System.out.println("\nHistory: " + am.toString());
+   System.out.println("Total: " + am.getTotal());
+
+   am.subtract(3);
+   System.out.println("\nHistory: " + am.toString());
+   System.out.println("Total: " + am.getTotal());
+
+   am.add(am.getTotal());
+   System.out.println("\nHistory: " + am.toString());
+   System.out.println("Total: " + am.getTotal());
+
+   am.clear();
+   System.out.println("\nHistory: " + am.toString());
+   System.out.println("Total: " + am.getTotal());
+  
   }
 }
 
